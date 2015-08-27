7e68f0f9515f0fb755db230ea819c9f49dbed1e1
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-08-27 14:15:15 -0700

    Add Fastly for cacheing

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'title', '~> 0.0.5'
 gem 'uglifier', '~> 2.7.1'
 gem 'unicorn', '~> 4.9.0'
 gem 'high_voltage', '~> 2.4.0'
+gem 'fastly-rails', '~> 0.4.0'
 
 gem 'celluloid', '0.16.0' # for sidekiq; 0.16.1 was yanked from Rubygems
 #                           and sidekiq is locked below 0.17.0
--- setup.rb
+++ setup.rb
@@ -1,5 +1,10 @@
 setup :secret_key_base
 env 'HOST', provide('Host', default: 'localhost:3000')
 env 'ASSET_HOST', provide('Asset host', default: 'localhost:3000')
-
+env 'FASTLY_API_KEY',
+    provide('Fastly API Key (can be nil in non-production environments)',
+            default: 'nil')
+env 'FASTLY_SERVICE_ID',
+    provide('Fastly Service ID (can be nil in non-production environments)',
+            default: 'nil')
 rake 'db:setup'
