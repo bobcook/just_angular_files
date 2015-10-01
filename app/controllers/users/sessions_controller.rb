0b0d5aadbd868ecf81f5441c627ddc937c8f9685
Alexis King <lexi.lambda@gmail.com>
2015-10-01 16:49:47 -0700

    Fix current_user being nil in the sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,12 @@
 module Users
   class SessionsController < Devise::SessionsController
     def destroy
+      # current_user is nil in super's context
+      user = current_user
+
       super do
         # consider backgrounding
-        Apis::DSO.endpoints.logout current_user.auth_token
+        Apis::DSO.endpoints.logout user.auth_token
       end
     end
   end
