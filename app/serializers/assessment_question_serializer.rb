b5dd42015d2243cdf120fdf997bbce3cc173f352
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-12-04 10:19:03 -0800

    fix: rename field in AssessmentQuestionSerializer

--- app/serializers/assessment_question_serializer.rb
+++ app/serializers/assessment_question_serializer.rb
@@ -1,4 +1,4 @@
 class AssessmentQuestionSerializer < ActiveModel::Serializer
-  attributes :id, :text, :answer_options, :recommendation_id, :order, :type,
-             :assessment_id, :answer_values
+  attributes :id, :text, :answer_options, :external_recommendation_id, :order,
+             :type, :assessment_id, :answer_values
 end
