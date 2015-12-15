41accd2d2a831e44c78787e9dc0ec6fdfd3d9ec7
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-12-15 15:31:04 -0800

    add Google Analytics using Segment.io

--- client/src/app/index.run.js
+++ client/src/app/index.run.js
@@ -1,8 +1,28 @@
-const runBlock = function ($log, $window, $rootScope) {
+const runBlock = function ($log, $window, $rootScope, $location) {
   'ngInject';
 
-  $rootScope.$on('$stateChangeSuccess', function (){
+  $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams){
+    // scroll to top of page
     $window.scrollTo(0, 0);
+
+    // begin of Segment.io analytics
+    const path = $location.path();
+    const url = $location.url();
+
+    const querystring = (url.indexOf('?') !== -1) ?
+      url.substring( url.indexOf('?'), url.length) : '';
+
+    const referrer = fromState.name ?
+      `${$location.protocol()}://${$location.host()}/#${fromState.url}` : '';
+
+    analytics.page({
+      path: path,
+      referrer: referrer,
+      search: querystring,
+      url: $location.absUrl(),
+    });
+    // end of Segment.io analytics
+
   });
 
   $log.debug('runBlock end');
