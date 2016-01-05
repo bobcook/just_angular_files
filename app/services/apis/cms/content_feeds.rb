73628d8e52bd91c6c84a393aa7e6b1b7b79deac4
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-01-05 14:28:04 -0800

    add 2016 cms urls

--- app/services/apis/cms/content_feeds.rb
+++ app/services/apis/cms/content_feeds.rb
@@ -2,22 +2,21 @@ module Apis
   module CMS
     class ContentFeeds
       CMS_BASE_URL = ENV.fetch('CMS_BASE_URL')
+      CONTENT_TYPES = %w(activities games recipes articles)
+      YEARS = %w(2015 2016)
 
       def fetch
-        [
-          {
-            'url': "#{CMS_BASE_URL}/activities/info-2015.infinity.json"
-          },
-          {
-            'url': "#{CMS_BASE_URL}/games/info-2015.infinity.json"
-          },
-          {
-            'url': "#{CMS_BASE_URL}/recipes/info-2015.infinity.json"
-          },
-          {
-            'url': "#{CMS_BASE_URL}/articles/info-2015.infinity.json"
-          }
-        ]
+        content_urls
+      end
+
+      private
+
+      def content_urls
+        YEARS.map do |year|
+          CONTENT_TYPES.map do |type|
+            { 'url': "#{CMS_BASE_URL}/#{type}/info-#{year}.infinity.json" }
+          end
+        end.flatten
       end
     end
   end
