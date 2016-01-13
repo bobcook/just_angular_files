9de43ebdef37d90c6d211fbd6a4406e463ceee3c
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-01-13 13:33:02 -0800

    clear elasticsearch indices for testing

--- spec/models/elasticsearch_shared_examples.rb
+++ spec/models/elasticsearch_shared_examples.rb
@@ -1,11 +1,11 @@
 shared_examples 'it calls ElasticsearchIndexer' do |factory|
-  it 'should call elasticsearch_indexer after save' do
+  it 'should call elasticsearch_indexer after save', elasticsearch: true do
     subject = build(factory)
     expect(subject).to receive(:elasticsearch_indexer)
     subject.save
   end
 
-  it 'should call elasticsearch_indexer after destroy' do
+  it 'should call elasticsearch_indexer after destroy', elasticsearch: true do
     subject = create(factory)
     expect(subject).to receive(:elasticsearch_indexer)
     subject.destroy!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 # http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
+  def indexed_resources
+    [Article, Activity, Game, Recipe]
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
   end
@@ -7,6 +12,27 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.syntax = :expect
     mocks.verify_partial_doubles = true
+  end
+
+  # prevent elasticsearch_indexer from executing
+  config.before :each do
+    indexed_resources.each do |resource|
+      allow_any_instance_of(resource).to receive(:elasticsearch_indexer)
+    end
+  end
+
+  # only enable elasticsearch_indexer for specs with elasticsearch condition
+  config.before :each, elasticsearch: true do
+    indexed_resources.each do |resource|
+      allow_any_instance_of(resource)
+        .to receive(:elasticsearch_indexer).and_call_original
+    end
+  end
+
+  config.after :each, elasticsearch: true do
+    indexed_resources.each do |resource|
+      resource.__elasticsearch__.client.indices.delete index: '*_test'
+    end
   end
 
   config.order = :random
