8552d3beecce50e1419f5bbb18d845219d3d5041
Alexis King <lexi.lambda@gmail.com>
2016-01-13 10:16:58 -0800

    Add a graph for quantity activity trackers
    - Always cap scale activity trackers at exactly 5

--- client/src/app/components/activity-tracker/quantity/quantity.controller.js
+++ client/src/app/components/activity-tracker/quantity/quantity.controller.js
@@ -1,4 +1,12 @@
 const ActivityTrackerQuantityController = function () {
+  this.maxResponse = () => {
+    if (this.type === 'scale') {
+      return 5;
+    } else {
+      return _.max(_.map(this.periods, (period) =>
+        period.activityTrackerResponses[0].response));
+    }
+  };
 };
 
 export default ActivityTrackerQuantityController;
