147ae27bbea1b4953e9e4fd9a0ea78d7bbef87d2
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-01-14 17:02:30 -0800

    add Adobe DTM analytics

--- client/src/app/index.run.js
+++ client/src/app/index.run.js
@@ -9,7 +9,7 @@ const runBlock = function ($log, $window, $rootScope, $location) {
     // scroll to top of page
     $window.scrollTo(0, 0);
 
-    // begin of Segment.io analytics
+    // begin of analytics
     const path = $location.path();
     const url = $location.url();
 
@@ -19,6 +19,11 @@ const runBlock = function ($log, $window, $rootScope, $location) {
     const referrer = fromState.name ?
       `${$location.protocol()}://${$location.host()}/#${fromState.url}` : '';
 
+    // begin of Adobe DTM analytics
+    _satellite.track(url);
+    // end of Adobe DTM analytics
+
+    // begin of Segment.io analytics
     analytics.page({
       path: path,
       referrer: referrer,
