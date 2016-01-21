300db47102f8ae70614b18d4d2885b19d0b4a020
Alex <alex@of-6.com>
2016-01-20 18:45:56 -0800

    Activity tracker styles * Update activity tracker quanity layout * Add logic to update graph * Update Edit modal layout

--- client/src/app/components/activity-tracker/quantity/quantity.controller.js
+++ client/src/app/components/activity-tracker/quantity/quantity.controller.js
@@ -1,4 +1,21 @@
 const ActivityTrackerQuantityController = function () {
+
+  const periods = this.periods;
+  this.activityResponseMax = () => {
+    const responses = _.map(
+      periods,
+      per => per.activityTrackerResponses[0].response
+    );
+    return Math.max.apply(Math, responses);
+  };
+
+  this.periodHeight = (num) => {
+    const percentage = (num / this.activityResponseMax()) * 100;
+    return {
+      'height': percentage + '%',
+    };
+  };
+
 };
 
 export default ActivityTrackerQuantityController;
