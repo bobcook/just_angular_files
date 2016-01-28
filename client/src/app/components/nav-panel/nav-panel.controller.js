966cc895a79acf3fe42ad35adeaa290cf2580e60
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-01-27 16:31:15 -0800

    show account link

--- client/src/app/components/nav-panel/nav-panel.controller.js
+++ client/src/app/components/nav-panel/nav-panel.controller.js
@@ -2,7 +2,7 @@ const NavPanelController = function (ApiRoutes, AssessmentStatus, $rootScope) {
   'ngInject';
 
   this.loginUrl = `${ApiRoutes.AARP_AUTH}?promo=SM-SS`;
-  this.currentRoute = 'root';
+  this.accountUrl = window.__env.accountUrl;
 
   if ($rootScope.$currentUser.isLoggedIn) {
     AssessmentStatus.hasCompletedAssessments().then((completed) => {
