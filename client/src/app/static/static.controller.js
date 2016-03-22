3f5e9ef3cf8cf765662f5748d969a2d4989c6219
khisakuni <kohei@gophilosophie.com>
2016-03-22 16:03:40 -0700

    - Redirect for paid games - Subscribe link point to correct environment

--- client/src/app/static/static.controller.js
+++ client/src/app/static/static.controller.js
@@ -1,8 +1,12 @@
 const StaticController = function ($location,
-                                   $anchorScroll) {
+                                   $anchorScroll,
+                                   $state,
+                                   dsoAuth) {
   'ngInject';
 
   $anchorScroll.yOffset = 80;
+
+  this.subscribeUrl = dsoAuth.dsoSubscribeAuth($state.href('application.home'));
 
   this.scrollTo = function (id) {
     $location.hash(id);
