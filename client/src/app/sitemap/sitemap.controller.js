fc9b184414925dfa974bbff3f46a91b9b724f571
khisakuni <kohei@gophilosophie.com>
2016-04-26 11:53:08 -0700

    Change included urls and order

--- client/src/app/sitemap/sitemap.controller.js
+++ client/src/app/sitemap/sitemap.controller.js
@@ -2,21 +2,14 @@ const SitemapController = function ($state, SitemapUrl) {
   'ngInject';
 
   const whiteListStates = [
+    ['static.what-is-staying-sharp', 'What is Staying Sharp'],
+    [null, 'FAQs', 'https://stayingsharp.zendesk.com/hc/en-us'],
+    ['assessments', 'Take the Assessment'],
     ['activities'],
     ['articles'],
-    ['assessments', 'Take the Assessment'],
-    ['static.privacy-policy', 'Privacy Policy'],
-    ['static.terms-of-service', 'Terms of Service'],
-    ['static.what-is-staying-sharp', 'What is Staying Sharp'],
-    ['user.articles', 'My Articles'],
-    ['user.assessments.overall', 'My Assessment Results'],
-    ['user.games', 'My Games'],
-    ['user.recipes', 'My Recipes'],
-    ['user.archive', 'My Archive'],
-    ['user.working-on', 'My activities'],
     ['games'],
     ['recipes'],
-    [null, 'FAQ', 'https://stayingsharp.zendesk.com/hc/en-us'],
+    ['user.working-on', 'My Staying Sharp'],
   ];
 
   const createSitemapUrls = (states) => {
