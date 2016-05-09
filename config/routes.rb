e1e7a1bb5010c1e3d797c2482830bb9100f26c97
Jordaniel Wolk <jd@gophilosophie.com>
2016-05-09 13:55:15 -0700

    Adds google verification file for server + removes API home page

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
            as: :destroy_user_session
   end
 
-  root to: 'home#show'
-  resource :home, controller: 'home', only: :show
   resources :activities, only: [:index, :show]
 
   resource :assessment, controller: 'assessment', only: [:show] do
