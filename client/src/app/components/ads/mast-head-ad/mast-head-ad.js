4640e01f3a692afe25afd5bdcd775af7970b6a16
ethanj <ethanj@gmail.com>
2016-06-08 11:19:04 -0700

    dont show leaderboard on some pages (#589)

--- client/src/app/components/ads/mast-head-ad/mast-head-ad.js
+++ client/src/app/components/ads/mast-head-ad/mast-head-ad.js
@@ -1,6 +1,14 @@
 import advertising from '../../../common/services/advertising';
 
-const mastHeadAd = function ($timeout) {
+const stateShouldHaveAds = (appStatePathname) => {
+  return !(
+    appStatePathname === '/assessments' ||
+    appStatePathname === '/what-is-staying-sharp' ||
+    appStatePathname.includes('/me/')
+  );
+};
+
+const mastHeadAd = function ($timeout, $location) {
   'ngInject';
   return {
     templateUrl: 'app/components/ads/mast-head-ad/mast-head-ad.html',
@@ -9,14 +17,24 @@ const mastHeadAd = function ($timeout) {
     scope: {
       adslot: '@',
     },
-    link: function (scope, element) {
-      advertising.linkDirective(scope, element, $timeout);
+    link: function (scope, element, attr) {
+      if (stateShouldHaveAds($location.path())) {
+        element.show();
+        advertising.linkDirective(scope, element, $timeout);
+      } else {
+        element.hide();
+      }
       if (scope.$root.userSeesAds()) {
-        scope.$root.$on('$stateChangeSuccess', function () {
-          googletag.cmd.push(function () {
-            googletag.pubads()
+        scope.$root.$on('$stateChangeSuccess', function (event, toState) {
+          if (stateShouldHaveAds($location.path())) {
+            element.show();
+            googletag.cmd.push(function () {
+              googletag.pubads()
               .refresh([advertising.adSlotsByID[scope.adslot]]);
-          });
+            });
+          } else {
+            element.hide();
+          }
         });
       }
     },
