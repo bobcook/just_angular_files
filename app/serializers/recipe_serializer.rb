591b8f14db17d984dd0cfb4aae8ab16556690ad5
Mark Li <mark@philosophie.is>
2016-06-13 17:28:05 -0700

    Show recipe brain health benefits (#601)

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,16 +1,16 @@
 class RecipeSerializer < ActiveModel::Serializer
   include PathUtils
 
-  delegate :card_image, :card_title, :description, :duration, :id,
-           :mast_head_image, :mast_head_title, :recommend_percentage,
-           :section1_body, :title,
+  delegate :benefits_to_brain_health, :card_image, :card_title, :description,
+           :duration, :id, :mast_head_image, :mast_head_title,
+           :recommend_percentage, :section1_body, :title,
            to: :common
 
-  attributes :card_image, :card_title, :description, :duration, :id,
-             :ingredients, :instructions, :mast_head_image, :mast_head_title,
-             :recommend_percentage, :section1_body, :slug,
-             :source_materials_citation, :path_pillar, :path_year,
-             :keywords, :seo_title, :seo_description, :canonical_url, :robots
+  attributes :benefits_to_brain_health, :card_image, :card_title, :description,
+             :duration, :id, :ingredients, :instructions, :mast_head_image,
+             :mast_head_title, :recommend_percentage, :section1_body, :slug,
+             :source_materials_citation, :path_pillar, :path_year, :keywords,
+             :seo_title, :seo_description, :canonical_url, :robots
 
   has_many :pillars
 
